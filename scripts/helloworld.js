d9c4a863a1b53fb6d8ce5df3aee06c95c8fa3a49
ahi_o <ahi_o@LAPTOP-OO3GIVR0>
2023-03-28 11:01:11 +0300

    valmis

---
+++ scripts/helloworld.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    console.log("hellohelllohelllo");
+}
+function sayHelloTo(name) {
+    alert("Hei " + name + "!");
+    console.log("sayHello-funktio suoritettu");
+}
