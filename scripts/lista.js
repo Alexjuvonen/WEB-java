170231a41e84c06588c9cdbd8e80ce1c36c97d38
ahi_o <ahi_o@LAPTOP-OO3GIVR0>
2023-04-12 13:33:43 +0300

    valmis

---
+++ scripts/lista.js
@@ -0,0 +1,7 @@
+function ShowList() {
+    document.getElementById('image-container').classList.remove('grid-container');
+}
+
+function ShowGrid() {
+    document.getElementById('image-container').classList.add('grid-container');
+}
